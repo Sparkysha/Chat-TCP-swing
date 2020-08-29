8b865e4f6c36ee1afde8b769f7d93dbdb2df8e47
Sparkysha <3376226@gmail.com>
2020-08-29 14:41:46 +0300

    Add modal [v0.51]

--- Server/src/sparky/chat/server/ServerGUI.java
+++ Server/src/sparky/chat/server/ServerGUI.java
@@ -4,9 +4,14 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ServerGUI extends JFrame {
+    private int port = 8585;
     private final JTextArea log = new JTextArea();
     private final JPanel panel = new JPanel();
     private final JButton start = new JButton("Start");
+
+    private final JTextField portInput = new JTextField();
+    private final JLabel label = new JLabel("Enter port:");
+    private final JButton ok = new JButton("OK");
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new ServerGUI());
@@ -23,7 +28,21 @@ public class ServerGUI extends JFrame {
         log.setLineWrap(true);
         add(log);
 
-        start.addActionListener(actionEvent -> new Thread(() -> new Server(8585, ServerGUI.this)).start());
+        portInput.setText("8585");
+        ok.addActionListener(actionEvent -> {
+            port = Integer.valueOf(portInput.getText());
+            dispose();
+        });
+        start.addActionListener(actionEvent -> {
+            JDialog dialog = new JDialog(this, true);
+            dialog.setSize(150, 150);
+            dialog.setLocationRelativeTo(null);
+            dialog.add(label, BorderLayout.NORTH);
+            dialog.add(portInput, BorderLayout.CENTER);
+            dialog.add(ok, BorderLayout.SOUTH);
+            dialog.setVisible(true);
+            new Thread(() -> new Server(port, ServerGUI.this)).start();
+        });
         panel.add(start);
         add(panel, BorderLayout.SOUTH);
     }
